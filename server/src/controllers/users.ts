cc015f00f047fe1451259531a6d1a9dc22f8b3a0
Je r Becerro <jearbecerro@gmail.com>
2023-07-18 14:20:55 +0800

    added router - ready for testing

--- server/src/controllers/users.ts
+++ server/src/controllers/users.ts
@@ -1,10 +1,20 @@
 import express from "express";
-import { deleteUserById, getUsers, updateUserById } from "db/users";
+import { deleteUserById, getUserById, getUsers, updateUserById } from "db/users";
+
+export const getUser = async (req: express.Request, res: express.Response) => {
+    try {
+        const { id } = req.body
+        const users = await getUserById(id);
+        return res.status(200).json(users);
+    } catch (error) {
+        console.log(error.message);
+        return res.sendStatus(400);
+    }
+};
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
     try {
         const users = await getUsers();
-
         return res.status(200).json(users);
     } catch (error) {
         console.log(error.message);
--- server/src/router/books.ts
+++ server/src/router/books.ts
@@ -1,2 +1,10 @@
 import express from 'express';
 
+import { isAuthenticated } from '../middlewares';
+import { deleteBook, getSaveBooks, saveBook } from 'controllers/books';
+
+export default (router: express.Router) => {
+  router.get('/books/:id', isAuthenticated, getSaveBooks);
+  router.delete('/books/:id', isAuthenticated, deleteBook);
+  router.post('/save-books', isAuthenticated, saveBook);
+};
