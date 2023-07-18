2032be53cdb75ed1efdef1a967f0c4d6c00307ec
Je r Becerro <jearbecerro@gmail.com>
2023-07-18 13:08:52 +0800

    DB Schema (users, books) added

---
+++ server/src/db/books.ts
@@ -0,0 +1,30 @@
+/**
+ * Save book per user
+ */
+import mongoose from 'mongoose';
+
+// Books Config
+const BooksSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    contributor: { type: String, required: true },
+    author: { type: String, required: true },
+    contributor_note: { type: String },
+    price: { type: String, required: true },
+    age_group: { type: String, required: true },
+    publisher: { type: String, required: true },
+    isbns: { type: [Object], required: true },
+    ranks_history: { type: Array, required: true },
+    reviews: { type: [Object], required: true },
+    user: {type: mongoose.Schema.Types.ObjectId, ref: "users"},
+});
+
+export const BooksModel = mongoose.model('Books', BooksSchema);
+
+// Books Actions
+export const getBooks = () => BooksModel.find();
+export const getBooksByEmail = (email: string) => BooksModel.findOne({ email });
+export const getBooksById = (id: string) => BooksModel.findById(id);
+export const createBooks = (values: Record<string, any>) => new BooksModel(values).save().then((Books) => Books.toObject());
+export const deleteBooksById = (id: string) => BooksModel.findOneAndDelete({ _id: id });
+export const updateBooksById = (id: string, values: Record<string, any>) => BooksModel.findByIdAndUpdate(id, values);
