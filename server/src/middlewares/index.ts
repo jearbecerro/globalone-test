49032dcbed4f57dd2308fe777193de2ffe5ad9d4
Je r Becerro <jearbecerro@gmail.com>
2023-07-18 14:10:22 +0800

    add Authentication

--- server/src/middlewares/index.ts
+++ server/src/middlewares/index.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import { verifyUserSessionToken } from 'helpers';
+import { merge } from 'lodash';
 
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction ) => {
     try {
+        const { sessionToken } = req.body;
         
+        if(!sessionToken){
+            return res.sendStatus(403);
+        }
+
+        const user = verifyUserSessionToken(sessionToken);
+        
+        merge(req, user);
+        return next();
     } catch (error) {
         console.log(error.message);
+        return res.sendStatus(400);
     }
 }
+
